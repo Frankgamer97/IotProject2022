2993ffb99299f85c179a663d640859134eaf94f9
primianocristino <primi.armi@libero.it>
2022-07-12 02:32:24 +0200

    v1.3

--- Project/oldv1.2/app.py
+++ Project/oldv1.2/app.py
@@ -13,17 +13,9 @@ from flask import Flask, make_response, request, redirect, url_for, abort, flash
 from flask_bootstrap import Bootstrap
 from bs4 import BeautifulSoup
 
-
-from datetime import datetime
-import os
-
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-
 post_parameters = {'sample_frequency': "5000",
              'min_gas_value': "0",
-             'max_gas_value': "10000",
+             'max_gas_value': "100",
              'protocol': "0"}
 
 
@@ -64,59 +56,6 @@ listvalues1 = [{ 'Device': "100",
                 'Gas': "100",
                 'AQI': "100"}]
 
-
-def is_int(data):
-    try:
-        isinstance(int(data), int)
-        return 1
-    except:
-        return 0
-    
-
-
-def get_protocol(prot):
-    if prot=="HTTP":
-        return 0
-    elif prot=="COAP":
-        return 1
-    elif prot=="MQTT":
-        return 2
-
-def influxdb_post(json_data):
-    
-    token = "7pTF08iW5yei6u8-8679-FnOPrLyjuBZm6l9mRbwTZZgwdqyMhjLRYUGm9axjZzVqppnSNU0gCkJ9JlPTUVgag=="
-    org = "primiarmi.pac@gmail.com"
-    bucket = "esp32"
-
-    client = InfluxDBClient(url="https://europe-west1-1.gcp.cloud2.influxdata.com", token=token, org=org)
-
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-
-
-    mac = json_data["MAC"]
-    GPS= json_data["GPS"]
-    rssi = json_data["RSSI"]
-    Temperature = json_data["Temperature"]
-    Humidity = json_data["Humidity"]
-    Gas = json_data["Gas"]
-    AQI = json_data["AQI"]
-    
-    print(json_data)
-    
-    point = Point("mem") \
-        .tag("Device", mac) \
-        .tag("GPS", GPS) \
-        .field("RSSI", rssi) \
-        .field("Temperature", Temperature) \
-        .field("Humidity", Humidity) \
-        .field("Gas", Gas) \
-        .field("AQI", AQI) \
-        .time(datetime.utcnow(), WritePrecision.NS)
-
-    #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-    write_api.write(bucket, org, point)
- 
-    return "ok"
 
 
 
@@ -189,16 +128,11 @@ def updatesensor():
                         'AQI': AQI
                         }
                        )
-    influxdb_post(json_data)
-
-    
     return "ok"
 
 #esp32 take values from the json post_parameters
 @app.route("/get-sensor/", methods=('GET', 'POST'))
 def getsensor():
-    #to write a json inizialize the variable with the name of the attribute
-    #ex: sample_frequency=100       ==>   {"sample_frequency":"100"}
     return jsonify(
         sample_frequency=post_parameters["sample_frequency"],
         min_gas_value=post_parameters["min_gas_value"],
@@ -214,35 +148,24 @@ def setparams():
         sample_frequency= request.form['sample_frequency']
         min_gas_value = request.form['min_gas_value']
         max_gas_value = request.form['max_gas_value']
-        protocol = request.form.get('comp_select')#request.form['protocol']
+        protocol = request.form['protocol']
         if not sample_frequency:
-            flash('frequency is required!'.upper())
-        elif not is_int(sample_frequency):
-            flash('frequency must be a number!'.upper())
+            flash('sample_frequency is required!')
         elif not min_gas_value:
-            flash('min gas value is required!'.upper())
-        elif not is_int(min_gas_value):
-            flash('min gas value must be a number!'.upper())
+            flash('min_gas_value is required!')
         elif not max_gas_value:
-            flash('max gas value is required!'.upper())
-        elif not is_int(max_gas_value):
-            flash('max gas value  must be a number!'.upper())
-       # elif not protocol:
-       #     flash('protocol is required!')
+            flash('max_gas_value is required!')
+        elif not protocol:
+            flash('protocol is required!')
         else:
             post_parameters['sample_frequency']= sample_frequency
             post_parameters['min_gas_value']= min_gas_value
             post_parameters['max_gas_value']= max_gas_value
-            post_parameters['protocol']= get_protocol(protocol)
+            post_parameters['protocol']= protocol
             flash('Parameters updated')
             #return redirect(url_for('getsensor'))
-            
-       # select = request.form.get('comp_select')
-       # print(select)
 
-
-    protocols=[{'name':'HTTP'}, {'name':'COAP'}, {'name':'MQTT'}]
-    return render_template('set_parameters.html',prot=protocols)
+    return render_template('set_parameters.html')
 
 
 
