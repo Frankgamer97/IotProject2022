7cfeebe65029fbb5ef7af0d8bf29797e8f06aaae
primianocristino <primi.armi@libero.it>
2022-08-01 12:08:58 +0200

    influxdb and meteo, arima still to be merged

--- Project/influxdb.py
+++ Project/influxdb.py
@@ -1,11 +1,16 @@
 from utility import influx_parameters 
+#from influxdb import DataFrameClient
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+from DataStorage import StorageHandler
+import pandas as pd
 
 
-def get_point(json_data,type_data,measurement="mamma"):
 
+def get_point(json_data,type_data,measurement="test-data1"):
+    print()#"----------------------INFLUX POST START----------------------")
     print(json_data)
+    print()#"----------------------INFLUX POST END----------------------")
 
     point = None
 
@@ -16,14 +21,47 @@ def get_point(json_data,type_data,measurement="mamma"):
         Time = json_data["Time"]
 
         point = Point(measurement) \
+            .tag("Device", "meteostat") \
             .tag("GPS", GPS) \
             .field("Temperature", Temperature) \
             .time(Time, WritePrecision.NS)
 
-    elif type_data == "forecasted_data":
-        pass
 
-    else:
+    elif type_data == "forecasted_meteostat":
+
+        device = json_data["meteostat_Device_predictions"]
+        GPS= json_data["meteostat_GPS_predictions"]
+        Temperature = json_data["meteostat_Temperature_predictions"]
+        Time = json_data["Time"]
+
+
+        
+        point = Point(measurement) \
+            .tag("Device", device) \
+            .tag("GPS", GPS) \
+            .field("Temperature_predicted", Temperature) \
+            .time(Time, WritePrecision.NS)
+
+
+    elif type_data == "forecasted_data":
+        mac = json_data["my_data_Device_predictions"]
+        GPS= json_data["my_data_GPS_predictions"]
+        Temperature = json_data["my_data_Temperature_predictions"]
+        Humidity = json_data["my_data_Humidity_predictions"]
+        Gas = json_data["my_data_Gas_predictions"]
+        Time = json_data["Time"]
+
+
+        
+        point = Point(measurement) \
+            .tag("Device", mac) \
+            .tag("GPS", GPS) \
+            .field("Temperature_predicted", Temperature) \
+            .field("Humidity_predicted", Humidity) \
+            .field("Gas_predicted", Gas) \
+            .time(Time, WritePrecision.NS)
+
+    elif type_data == "my_data":
         mac = json_data["MAC"]
         GPS= json_data["GPS"]
         rssi = json_data["RSSI"]
@@ -47,9 +85,7 @@ def get_point(json_data,type_data,measurement="mamma"):
 
     return point
 
-
-
-def influxdb_post(json_data, type_data="my_data", measurement=""):
+def influxdb_post_copy(json_data, type_data="my_data", measurement="",single_point=True,tag_columns=[],field_columns =[]):
     
 
     user= influx_parameters["user"]
@@ -57,21 +93,239 @@ def influxdb_post(json_data, type_data="my_data", measurement=""):
     bucket= influx_parameters["bucket"]
     server= influx_parameters["server"]
     if measurement=="":
-        print("sono qui")
         measurement= influx_parameters["measurement"]
 
+    if single_point:   
+
+        client = InfluxDBClient(url=server, token=token, org=user)
+     
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        point = get_point(json_data,type_data,measurement)
+
+        #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+        write_api.write(bucket, user, point)
+    else:
 
 
-    client = InfluxDBClient(url=server, token=token, org=user)
+        client = InfluxDBClient(url=server, token=token, org=user)
+     
+        write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    write_api = client.write_api(write_options=SYNCHRONOUS)
+        #point = get_point(json_data,type_data,measurement)
 
-    
-    point = get_point(json_data,type_data,measurement)
+        #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+        #print(json_data)
+
+        
 
 
-    #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-    write_api.write(bucket, user, point)
+
+        #write_api.write(bucket=bucket, org=user, record=json_data,data_frame_measurement_name=measurement,data_frame_tag_columns=tag_columns,data_frame_timestamp_column=["Time"])
+
+
+    #    client = DataFrameClient(url=server, token=token, org=user)
+    #    protocol = 'json'
+    #    client.write_points(json_data, measurement, protocol=protocol,tag_columns=tag_columns,field_columns =field_columns)
  
     return "ok"
 
+def influxdb_post_1(json_data, type_data="my_data", measurement="",single_point=True,tag_columns=[],field_columns =[]):
+    
+
+    user= influx_parameters["user"]
+    token= influx_parameters["token"]
+    bucket= influx_parameters["bucket"]
+    server= influx_parameters["server"]
+    if measurement=="":
+        measurement= influx_parameters["measurement"]
+
+
+    if single_point:   
+        print("NOOOOOOOOOOO")
+
+        client = InfluxDBClient(url=server, token=token, org=user)
+     
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        point = get_point(json_data,type_data,measurement)
+
+        #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+        write_api.write(bucket, user, point)
+    else:
+        print("SIIIIIIIIIII")
+
+        client = InfluxDBClient(url=server, token=token, org=user)
+     
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        #point = get_point(json_data,type_data,measurement)
+
+        #point = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+        #for el in json_data:
+        #    print(el)
+
+        print(json_data)
+        table=pd.DataFrame(json_data)
+        print(table)
+        '''
+        dt2=pd.to_datetime(table["Time"],format="%d/%m/%Y",unit='ns')
+        dta=pd.date_range("2020-12-1","2022-07-1",freq="D")
+        print("TYEP")
+        print(type(dt2))
+        print(type(dta))
+        print(dt2)
+        table["Time"]=dt2
+      
+
+        #table.set_index('Time', inplace=True)# Transform index to PeriodIndex
+        #table.index = pd.to_datetime(table.index, unit='s')
+        #table.index.name="Time"
+
+        #ts = pd.to_datetime(str(dt2)) 
+        d = dta.strftime('%d-%m-%Y')
+        print("----------------------------------------------------------------------------------------------------------")
+        print((d))
+        table["Time"]=dta
+        #table["Time"]=d
+        print(table)
+        print(measurement)
+'''
+
+
+        write_api.write(bucket=bucket, org=user, record=table,data_frame_measurement_name=measurement,data_frame_tag_columns=tag_columns,data_frame_timestamp_column="Time")
+
+
+    #    client = DataFrameClient(url=server, token=token, org=user)
+    #    protocol = 'json'
+    #    client.write_points(json_data, measurement, protocol=protocol,tag_columns=tag_columns,field_columns =field_columns)
+ 
+    return "ok"
+
+
+
+
+
+
+def influxdb_post(json_data, measurement="",tag_col=[],time_col ="Time"):
+    
+
+    user= influx_parameters["user"]
+    token= influx_parameters["token"]
+    bucket= influx_parameters["bucket"]
+    server= influx_parameters["server"]
+    if measurement=="":
+        measurement= influx_parameters["measurement"]
+
+
+    client = InfluxDBClient(url=server, token=token, org=user)
+ 
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    print(json_data)
+
+
+    write_api.write(bucket=bucket, org=user, record=json_data,data_frame_measurement_name=measurement,data_frame_tag_columns=tag_col,data_frame_timestamp_column=time_col)
+
+    return "ok"
+
+def influxdb_query(measurement=""):
+    user= influx_parameters["user"]
+    token= influx_parameters["token"]
+    bucket= influx_parameters["bucket"]
+    server= influx_parameters["server"]
+    if measurement=="":
+        measurement= influx_parameters["measurement"]
+
+
+    client = InfluxDBClient(url=server, token=token, org=user)
+    query_api = client.query_api()
+
+    query = """from(bucket: "esp32")
+      |> range(start: -30d)
+      |> filter(fn: (r) => r["_measurement"] == "Weather")
+      |> filter(fn: (r) => r["GPS"] == "44.49-11.313")
+      |> filter(fn: (r) => r["ID"] == "1")
+      |> filter(fn: (r) => r["_field"] == "AQI" or r["_field"] == "gas" or r["_field"] == "humidity" or r["_field"] == "temperature" or r["_field"] == "wifi")
+      |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value" )
+      |> keep(columns: ["Device", "GPS", "RSSI" ,"Temperature",  "Humidity", "Gas","AQI","_time"])
+      """
+
+  #  query_api = client.query_api()
+
+    query = f' from(bucket:"{influx_parameters["bucket"]}")\
+    |> range(start: -30d)\
+    |> filter(fn:(r) => r["_measurement"] == "{measurement}")\
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value" )\
+    |> keep(columns: ["Device", "GPS", "RSSI" ,"Temperature",  "Humidity", "Gas","AQI","_time"])'
+
+    #print(query)
+
+
+    tables = query_api.query_data_frame(query=query, org=user)
+    #print(type(tables))
+
+
+
+    try:
+        StorageHandler().save_data_csv(tables, ["Device", "GPS", "RSSI" ,"Temperature",  "Humidity", "Gas","AQI","_time"] ,measurement)
+    except:
+        pass
+    return tables
+
+'''
+
+def influxdb_get(measurement="",drop_columns=[],masking_device=None):
+
+    table=influxdb_query(measurement).drop(columns=drop_columns, axis=1, errors='ignore')
+    table=table.rename(columns={"_time":"ds"})
+    table_notime=table.drop(columns=["_time"], axis=1, errors='ignore')
+
+
+    if masking_device!= None:
+        mask = table['Device'].str.contains(masking_device)
+        table = table[mask]
+        table_NonAccl = table[~mask]
+    table= table.set_index('ds')#.drop(columns=["Device"], axis=1, errors='ignore')
+    return table
+
+'''
+
+def dataframe2series_list(df,name):
+
+    list_series=[]
+    for col in df:
+        list_series.append(df[col].squeeze().rename(col))
+
+    return list_series
+
+
+
+
+def get_dataframe_from_influxdb(measurement, drop_columns=["AQI","result","table","RSSI"],masking_device=None,name="my_data_"):
+    table=influxdb_query(measurement).drop(columns=drop_columns, axis=1, errors='ignore')
+    table=table.rename(columns={"_time":"ds"})
+    table_notime=table.drop(columns=["_time"], axis=1, errors='ignore')
+    #print(table)
+    #print(table_notime)
+    if masking_device!= None:
+        mask = table['Device'].str.contains(masking_device)
+        table = table[mask]
+        #table_NonAccl = table[~mask]
+
+    df= table.set_index('ds')#.drop(columns=["Device"], axis=1, errors='ignore')
+    df=df.dropna()
+    #print(df)
+
+
+    return dataframe2series_list(df,name)#list_series
+
+
+if __name__=="__main__":
+
+    list_series=get_dataframe_from_influxdb(measurement="test-july27-3")
+    for serie in list_series:
+        print()
+        print(serie)
+        print(serie.shape[0])
+        print()
--- Project/meteo.py
+++ Project/meteo.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 from utility import listvalues
 import pandas as pd
-from influxdb import influxdb_post
+from influxdb import influxdb_post, get_dataframe_from_influxdb
+from arima_model import Forecast, get_predictions_list
 
 
-
+			
 
 class Meteo:
-	def __init__(self):
+	def __init__(self,measurement):
 		self.lat = None
 		self.long = None
 		self.start = None
 		self.start = None
 		self.location = None
 		self.data = None
-
+		self.measurement=measurement
 	def get_coord(self):
 		if len(listvalues) > 0:
 			self.lat =listvalues[0]["GPS"][0]
@@ -24,7 +25,6 @@ class Meteo:
 		else:
 			print("gps default: Bologna")
 			self.lat, self.long=44.497612, 11.353733
-			
 
 	def set_time(self,start,end):
 		self.start=datetime(start[0],start[1],start[2])
@@ -49,26 +49,235 @@ class Meteo:
 			print("no data to visualize")
 
 
-	def get_data(self):
+	def get_data_raw(self):
 		tavg = self.data.to_dict()["tavg"]
 		new_data_list = [] 
 		for k, v in tavg.items():
-			new_data_list.append({"Time":k,"Temperature":v,"GPS":[self.lat, self.long]})
+			new_data_list.append({"Time":k,"GPS":[self.lat, self.long],"Temperature":v})
+
+		return new_data_list
+
+	def build_dataframe(self,start,end,measurement=None):
+		if measurement is not None:
+			self.measurement=measurement
+		self.get_interval_meteo(start,end)
+		return self.get_data_raw()
+
+
+
+	def post_data_raw(self, measurement=None):
+		if measurement is not None:
+			self.measurement=measurement
+
+		lista=self.get_data_raw()
+		'''
+		table=pd.DataFrame(self.get_data_raw())
+		dte=pd.to_datetime(table["Time"])
+		print(dte)
+		print(table)
+		#print()
+		dta=pd.date_range("2020-12-1","2022-07-1",freq="D")
+		print(dta)
+		#print("hey")
+		#dte = pd.to_datetime(dte,dayfirst=True)
+		#print("hoy")
+		#print(dte)
+		table["Time"]=dta
+		print(table)
+		#table=table.set_index('Time')
+		'''
+		#for el in lista:
+		influxdb_post(pd.DataFrame(lista), measurement=self.measurement, tag_col=["GPS"])
+
+	'''
+	def post_meteo(self, start, end,measurement=None):
+		if measurement is not None:
+			self.measurement=measurement
+		self.get_interval_meteo(start,end)
+		self.post_data_raw()
+	'''
+
+
+'''
+	def get_meteo(self):
+	        return get_dataframe_from_influxdb(self.measurement,name="Meteostat_")
+
+	def get_pred_list(self,series_list):
+	        return get_predictions_list(series_list)
+
+
+	def get_predicted_meteostat(self):
+	        series_list=self.get_meteo()
+
+	        prediction_list=self.get_pred_list(series_list)
+
+
+	        df_gps={}
+	        for df in list_series:
+	                if "GPS" in df.name:
+	                      df_gps=df
+
+
+	        df_gps_predictions=pd.Series(list(df_gps), index=prediction_list[0].index).rename(df_gps.name+"_predicted")
+
+	        df_total=pd.concat([df_gps_predictions,prediction_list[0]],axis=1)
+	        print("--------------------------------------------------->")
+	        df_total.reset_index(inplace=True)
+	        df_total = df_total.rename(columns = {'index':'Time'})
+	        self.meteo_predicted=df_total
+	        return df_total
+
+	def post_predictions(self):
+	        for i in range(self.meteo_predicted.shape[0]):
+	                #print(dict(self.meteo_predicted.iloc[i]))
+	                influxdb_post(pd.DataFrame(dict(self.meteo_predicted.iloc[i])), type_data="forecasted_meteostat", measurement=self.measurement)
+	        
+'''
+
+
+class MeteoPredictor(Meteo):
+
+
+	@staticmethod
+	def meteo2pd(df):
+	    df1 =df.copy()
+	    df1 = pd.DataFrame({ "y": df})
+	    df1.reset_index(inplace=True)
+	    df1 = df1.rename(columns = {'time':'ds'})
+	    return df1
+
+	@staticmethod
+	def pd2series(df,name_series="y"):
+	    df1=df.copy()
+	    df1= df1.set_index('ds')
+	    df1.index.name="ds"
+	    return df1.squeeze().rename(name_series)
+
+	@staticmethod
+	def series2pd(df):
+		df=df.to_frame()
+		df.reset_index(inplace=True)
+		df=df.rename(columns={"ds":"Time","Meteostat_Temperature_predicted":"Temperature_predicted"})
+		return df
+
+	@staticmethod
+	def series2pd_indexed(df):
+		df=df.to_frame()
+
+		return df	
+
+	def get_data_pred(self):
+		tavg = MeteoPredictor.series2pd_indexed(self.predictions).to_dict()["Meteostat_Temperature_predicted"]
+		new_data_list = [] 
+		for k, v in tavg.items():
+			new_data_list.append({"Time":k,"GPS":[self.lat, self.long],"Temperature_predicted":v})
+			#print(k,v)
 
 		return new_data_list
 
 
-	def post_data(self):
-		lista=self.get_data()
-		for el in lista:
-			influxdb_post(el, type_data="meteostat", measurement="test1-meteostat")
+
+	def predict(self,n_periods=365):
+		#start, end = (2020, 12, 1), (2022, 7, 1)
+		#meteor=Meteo(measurement="testFinal2-meteostat")
+		#meteor.get_interval_meteo(start,end)
+		##meteor.plot_data()
+
+		df =self.data.tavg.dropna()
+
+		df_pd = MeteoPredictor.meteo2pd(df)
+		df = MeteoPredictor.pd2series(df_pd,"Meteostat"+"_Temperature")
+
+		# print(df)
+		# print(type(df))
+
+		forcast=Forecast(df)
+		forcast.tuning()
+		forcast.fit(df.name)
+		self.predictions=forcast.forecast(df.name,n_periods)
+		# forcast.plot_forecast()
+
+		self.predictions.index.name="ds"
+		# print(self.predictions)
+		return self.predictions
+
+	def post_data_pred(self,measurement=None):
+		if measurement is not None:
+			self.measurement=measurement
+		lista=self.get_data_pred()
+		influxdb_post(pd.DataFrame(lista), measurement=self.measurement,tag_col=["GPS"])
+
+
+
+
+
+def main_meteostat_backup():
+	        start, end = (2020, 12, 1), (2022, 7, 1)
+	        meteor=Meteo(measurement="testFinal2-meteostat")
+	        meteor.get_interval_meteo(start,end)
+	        #meteor.plot_data()
+
+	        df =meteor.data.tavg.dropna()
+	        #df.index = pd.to_datetime(df.index)
+	        #df_shape =df.shape[0]
+	        #df =df.squeeze()
+	        df_pd = meteo2pd(df)
+	        df = pd2series(df_pd,"meteostat"+"_Temperature")
+
+	        print(df)
+	        print(type(df))
+
+	        forcast=Forecast(df)
+	        #print("SEASON",forcast.D)
+	        #print("STATION",forcast.d)
+	        forcast.tuning()
+	        forcast.fit(df.name)
+	      
+	        predictions=forcast.forecast(df.name,Forecast.seasonality)
+	        # forcast.plot_forecast()
+	        predictions.index.name="ds"
+	        return predictions
 
 
 if __name__ == '__main__':
-	meteor = Meteo()
-	meteor.get_interval_meteo((2022, 7, 1),(2022, 7, 16))
-	lista=meteor.get_data()
-	meteor.post_data()
+	
+	#meteor = Meteo()
+	#meteor.get_interval_meteo((2022, 7, 1),(2022, 7, 16))
+	# lista=meteor.get_data_raw()
+	#meteor.post_data_raw(measurement="test1-meteostat")
+
+#	meteor =Meteo(measurement="testFinal3-meteostat")
+	#meteor.get_interval_meteo((2020, 12, 1), (2022, 7, 1))
+	#table=pd.DataFrame(meteor.get_data_raw())
+	#table=table.set_index('Time')
+	#print(table)
+
+#	meteor.post_meteo( (2020, 12, 1), (2022, 7, 1))
+
+
+	#series_list=meteor.get_meteo()
+	#for df in series_list:
+#		print(df)
+	#meteor.get_predicted_meteostat()
+	#meteor.post_predictions()
+
+	meteor = MeteoPredictor(measurement="testFinal5-meteostat")
+	meteor.build_dataframe(start=(2020, 12, 1),end=(2022, 7, 1))
+	meteor.post_data_raw()
+	meteor.predict()
+	meteor.post_data_pred()
+
+
 	
 
+'''
+	predictions=main_meteostat()
+	predictions=predictions.to_frame()
+	predictions.reset_index(inplace=True)
+	predictions=predictions.rename(columns={"ds":"Time","meteostat_Temperature_predictions":"Temperature_predicted"})#meteostat_Temperature_predictions
+	#influxdb_post(predictions, type_data="meteostat_predicted", measurement=self.measurement,single_point=False,tag_columns=["GPS"],field_columns =["Temperature_predicted"])
+	
+	print(predictions)
+	print(type(predictions))
+'''
 
